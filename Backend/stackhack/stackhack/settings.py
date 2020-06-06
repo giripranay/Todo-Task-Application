3e10da4be93aa37a47f60b8537b7126993e143f9
Giri Pranay K| Dvara Solutions <giripranay.k@Sagars-MacBook-Air.local>
2020-06-06 22:35:46 +0530

    Allow all origins

--- Backend/stackhack/stackhack/settings.py
+++ Backend/stackhack/stackhack/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 #CORS validation
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
        'http://127.0.0.1:8080',
 )
